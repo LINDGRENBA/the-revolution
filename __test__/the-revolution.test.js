c207ec48e482b6c8170a05e6bf6b99c60be39041
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-24 14:36:39 -0700

    Change __test__/RBG.tst.js file name to __test__/the-revolution.test.js, add first test with result of fail.
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

---
+++ __test__/the-revolution.test.js
@@ -0,0 +1,14 @@
+import { Animal } from '../src/the-revolution.js';
+
+describe('Animal', () => {
+  
+  test('should correctly create an animal instance with properties assigned', () => {
+    const pig = new Animal(5, 7, 3, 9, 1, "food");
+    expect(pig.charm).toEqual(0);
+    expect(pig.wisdom).toEqual(7);
+    expect(pig.snark).toEqual(3);
+    expect(pig.tastiness).toEqual(9);
+    expect(pig.level).toEqual(1);
+    expect(pig.title).toEqual("food");
+  });
+});
---
+++ src/the-revolution.js
@@ -0,0 +1,27 @@
+export class Animal {
+  constructor (charm, wisdom, snark, tastiness, level, title) {
+    this.attributes = []
+    this.charm = charm;
+    this.wisdom = wisdom;
+    this.snark = snark;
+    this.tastiness = tastiness;
+    this.level = level;
+    this.title = title;
+  }
+
+}
+
+//  switch case 1: let chicken = new Animal (5,2,4,level 1, food)
+
+// switch case 2: let pig = new Animal (1, 2, 3, 4, )
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
