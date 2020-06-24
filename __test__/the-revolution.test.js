05d615d232af96ade3c99e99d701203507923e0d
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-24 14:44:56 -0700

    pass first constructor test
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- __test__/the-revolution.test.js
+++ __test__/the-revolution.test.js
@@ -4,7 +4,7 @@ describe('Animal', () => {
   
   test('should correctly create an animal instance with properties assigned', () => {
     const pig = new Animal(5, 7, 3, 9, 1, "food");
-    expect(pig.charm).toEqual(0);
+    expect(pig.charm).toEqual(5);
     expect(pig.wisdom).toEqual(7);
     expect(pig.snark).toEqual(3);
     expect(pig.tastiness).toEqual(9);
