a5e6f872dff42e9c3c554d61b022d8078b37a32a
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-24 16:03:39 -0700

    add test for human character creation
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- __test__/the-revolution.test.js
+++ __test__/the-revolution.test.js
@@ -2,7 +2,7 @@ import { Animal } from '../src/the-revolution.js';
 
 describe('Animal', () => {
   
-  test('should correctly create an animal instance with properties assigned', () => {
+  test('should correctly create an animal object with properties assigned', () => {
     const pig = new Animal(5, 7, 3, 9, 1, "food");
     expect(pig.charm).toEqual(5);
     expect(pig.wisdom).toEqual(7);
@@ -12,8 +12,12 @@ describe('Animal', () => {
     expect(pig.title).toEqual("food");
   });
 
-  test('should return true if human object is created', () => {
-    const human = new Animal();
-    expect(human.addHumanChar()).toEqual(true);
-  });
+  // test('should return the correct assigned property for a human object', () => {
+  //   let human = new Animal()
+  //   // addHumanChar().charm;
+  //   expect(addHumanChar().charm).toEqual(0);
+  // });
+
+
+  //human object should exist if animal object has been made
 });
