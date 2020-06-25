5274b57bd07c478030f6a1ecb1ebf3b322e18e9f
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-25 11:45:08 -0700

    Pass second test.
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- __test__/the-revolution.test.js
+++ __test__/the-revolution.test.js
@@ -3,15 +3,14 @@ import { Character } from '../src/the-revolution.js';
 describe('Character', () => {
   
   test('should correctly create an animal instance', () => {
-    const pig = new Character("pig", 5, 7, 3, 9, 1, "food");
-    expect(pig).toMatchObject({type: "pig", charm: 5, wisdom:7, snark: 3, tastiness: 9, level: 1, title: "food"});
+    const pig = new Character("pig");
+    expect(pig).toMatchObject({type: "pig"});
   });
 
   test('should assign different properties for different types', () => {
-
     const chicken = new Character("chicken");
     chicken.checkCharacterType();
-    expect(chicken.charm).toEqual(5);
+    expect(chicken.charm).toEqual(3);
   });
 
   // test('should compare stats for tastiness', () => {
