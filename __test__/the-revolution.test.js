046cb45e547017853a5471dd58014176b1dcc9a3
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-25 10:07:56 -0700

    test to create character object passing.
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- __test__/the-revolution.test.js
+++ __test__/the-revolution.test.js
@@ -4,13 +4,6 @@ describe('Character', () => {
   
   test('should correctly create an animal instance with properties assigned', () => {
     const pig = new Character("pig", 5, 7, 3, 9, 1, "food");
-    expect(pig).toMatchObject({type: "pig", charm:0, wisdom:7, snark: 3, tastiness: 9, level: 1, title: "food"});
-    // expect(pig.type).toEqual("pig");
-    // expect(pig.charm).toEqual(0);
-    // expect(pig.wisdom).toEqual(7);
-    // expect(pig.snark).toEqual(3);
-    // expect(pig.tastiness).toEqual(9);
-    // expect(pig.level).toEqual(1);
-    // expect(pig.title).toEqual("food");
+    expect(pig).toMatchObject({type: "pig", charm: 5, wisdom:7, snark: 3, tastiness: 9, level: 1, title: "food"});
   });
 });
